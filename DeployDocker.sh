8d7d62a902bd518dda95d52f67422b918afca718
Mahbubi Hamdani <47600643+mahbubi@users.noreply.github.com>
2022-11-04 10:12:52 +0700

    Update Bug Echo

--- DeployDocker.sh
+++ DeployDocker.sh
@@ -3,17 +3,17 @@
 sudo apt-get update
 echo "################ Instal Package to use a repository over HTTPS ################"
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
-echo"Done!"
+echo "Done!"
 echo "################ Add Docker official GPG key ################"
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo"Done!"
+echo "Done!"
 echo "################ set up the repository ################"
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-echo"Done!"
+echo "Done!"
 echo "################ Install Docker CE ################"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-echo"Done!"
-echo"Lets Check Your Current Installation"
+echo "Done!"
+echo "Lets Check Your Current Installation"
 sudo docker -v
